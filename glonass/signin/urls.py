ef0c1f6adb32d218923e0af6aa5baa435ef93afd
joshua2020181 <josh.cheng.sh@gmail.com>
2021-11-11 16:59:36 -0500

    set up urls.py

--- glonass/signin/urls.py
+++ glonass/signin/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('signin/', include('signin.urls')),
+    path('', views.index, name='index'),
 ]
