334ad893459fd7c73033422b7b571c00d6d23d8e
Joshua Cheng <cheng.jo@northeastern.edu>
2021-12-09 23:11:11 -0500

    updated views

--- glonass/signin/views.py
+++ glonass/signin/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from main.models import Preferences
 
 # Create your views here.
 
@@ -6,6 +7,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
+from django.db import connection
 
 def signin(request):
     print(request.POST)
@@ -27,9 +29,16 @@ def createAccount(request):
         if password != request.POST['confirm']:
             return render(request, "create.html", {"errormsg": "Passwords don't match"})
         # check if username is duplicate
+        with connection.cursor() as cur:
+            cur.execute("SELECT username_exists(%s);", [username])
+            if cur.fetchone()[0]: # if username exists
+                return render(request, "create.html", {"errormsg": "Username already exists, try signing in"})
+
         User.objects.create_user(username=username, password=password)
         user = authenticate(username=username, password=password)
         if user is not None:  # prob not needed
             login(request, user)
+            p = Preferences(user=user)
+            p.save()
             return redirect('/')
     return render(request, "create.html")
