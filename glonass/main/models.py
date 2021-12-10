1b81f809e3d23708644260484706a072dcee0f5a
Joshua Cheng <cheng.jo@northeastern.edu>
2021-12-09 23:59:23 -0500

    call procedures

--- glonass/main/models.py
+++ glonass/main/models.py
@@ -4,20 +4,25 @@ from django.contrib import admin
 
 # Create your models here.
 
+
 class Preferences(models.Model):
-    user = models.OneToOneField(User, verbose_name=("user"), on_delete=models.RESTRICT, primary_key=True)
-    galaxies = models.BooleanField(default=True)
-    stars = models.BooleanField(default=True)
-    planets = models.BooleanField(default=True)
-    blackHoles = models.BooleanField(default=True)
-    moons = models.BooleanField(default=True)
+    user = models.OneToOneField(User, verbose_name=(
+        "user"), on_delete=models.RESTRICT, primary_key=True)
+    survey_star = models.BooleanField(default=True)
+    high_magnitude = models.BooleanField(default=True)
+    high_variability = models.BooleanField(default=True)
+    low_variability = models.BooleanField(default=True)
+    multi_star_system = models.BooleanField(default=True)
+
 
 class UsersLocations(models.Model):
-    user = models.ForeignKey(User, verbose_name=("user"), on_delete=models.RESTRICT)
+    user = models.ForeignKey(User, verbose_name=(
+        "user"), on_delete=models.RESTRICT)
     label = models.CharField(max_length=255)
     primary = models.BooleanField(default=False)
     latitude = models.FloatField(default=0.0)
     longitude = models.FloatField(default=0.0)
+
 
 admin.site.register(Preferences)
 admin.site.register(UsersLocations)
--- glonass/main/views.py
+++ glonass/main/views.py
@@ -19,11 +19,11 @@ def preferences(request):
 
     if request.POST:
         print(request.POST)
-        data.galaxies = 'galaxies' in request.POST
-        data.stars = 'stars' in request.POST
-        data.planets = 'planets' in request.POST
-        data.blackHoles = 'blackHoles' in request.POST
-        data.moons = 'moons' in request.POST
+        data.survey_star = 'survey_star' in request.POST
+        data.high_magnitude = 'high_magnitude' in request.POST
+        data.high_variability = 'high_variability' in request.POST
+        data.low_variability = 'low_variability' in request.POST
+        data.multi_star_system = 'multi_star_system' in request.POST
         data.save()
         return render(request, "preferences.html", {"user": request.user, "data": data, "successmsg": "Success"})
 
@@ -32,7 +32,23 @@ def preferences(request):
 
 @login_required
 def app(request):
-    return render(request, "app.html", {"user": request.user})
+    pref = Preferences.objects.get(user=request.user)
+    data = []
+    with connection.cursor() as cur:
+        cur.callproc('get_survey_stars', []) if pref.survey_star else None
+        data.append(cur.fetchall())
+        cur.callproc('get_high_mag_stars', []) if pref.high_magnitude else None
+        data.append(cur.fetchall())
+        cur.callproc('get_high_var_stars', []
+                     ) if pref.high_variability else None
+        data.append(cur.fetchall())
+        cur.callproc('get_low_var_stars', []) if pref.low_variability else None
+        data.append(cur.fetchall())
+        cur.callproc('get_multi_stars', []) if pref.multi_star_system else None
+        data.append(cur.fetchall())
+    print(data)
+
+    return render(request, "app.html", {"user": request.user, "data": data})
 
 
 @login_required
